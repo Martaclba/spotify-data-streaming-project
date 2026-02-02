b7fae75386a0b78c2615a6e38dafdd0bd04b7dfc
Martaclba <martaclba@hotmail.com>
2026-02-02 12:05:53 +0000

    refactor: changed table schema for recently_played, artists, tracks and albums

--- src/bronze/bronze_config.py
+++ src/bronze/bronze_config.py
@@ -5,11 +5,37 @@ INGESTION_CONFIG = [
         "source": "streaming_history",
         "path": f"{BASE_PATH}/Streaming_History_Audio*.json",
         "format": "json",
-        "table": "workspace.bronze.spotify_streaming_history_raw"
+        "table": "workspace.bronze.spotify_streaming_history_raw",
+        "mode": "overwrite"
     },
     {
         "source": "api",
         "format": "api_call",
-        "table": "workspace.bronze.spotify_api_recently_played_tracks_raw"
+        "table": "workspace.bronze.spotify_recently_played_raw",
+        "mode": "append"
     }
 ]
+
+metadata_configs = {
+    "track": {
+        "table": "workspace.bronze.spotify_tracks_raw", 
+        "chunk": 50,
+        "api": "sp.tracks",
+        "key": "tracks",
+        "schema": "track_schema"
+    },
+    "artist": {
+        "table": "workspace.bronze.spotify_artists_raw", 
+        "chunk": 50,
+        "api": "sp.artists",
+        "key": "artists",
+        "schema": "artist_schema"
+    },
+    "album": {
+        "table": "workspace.bronze.spotify_albums_raw",
+        "chunk": 20,
+        "api": "sp.albums",
+        "key": "albums",
+        "schema": "album_schema"
+    }
+}
